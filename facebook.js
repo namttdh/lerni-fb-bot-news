b6a2a9520c866cb3a01a090e19823140a5b34019
Ducky9x <42636533+Ducky9x@users.noreply.github.com>
2020-04-28 11:20:35 +0700

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -48,6 +48,7 @@ function Facebook(accessToken, groupId)
             `# TIN TỨC BUỔI TRƯA ${date}\n` +
             '\n' +
             '## thông tin\n' +
+            '* #SUS_NEW\n' +               
             '* Bài viết được tự động đăng lúc 12 giờ trưa hàng ngày\n' +
             '* Đây là post tự động được tạo bởi Lerni và lấy dữ liệu từ VnExpress qua RSS các bạn có thể check source code [tại đây](https://bit.ly/lerni-bot-fb)');
         formPublish.append('formatting', 'MARKDOWN');
